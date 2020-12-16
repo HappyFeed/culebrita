e166a7470b7c63e19946970657394a5606d0d88b
HappyFeed <alejomam2011@gmail.com>
2020-12-15 23:20:00 -0500

    server config

--- main.go
+++ main.go
@@ -2,8 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+
+	"github.com/go-chi/chi"
 )
 
+var templates = template.Must(template.New("T").ParseGlob("ui/*.html"))
+
 func main() {
-	fmt.Print("Hello world")
+
+	r := chi.NewRouter()
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "hola")
+		//RenderTemplate(w, "ui/menu", nil)
+	})
+	//r.Get("/Scores")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
+
+/*func RenderTemplate(w http.ResponseWriter, name string, data interface{}) {
+	w.Header().Set("Content-Type", "text/html")
+
+	err := templates.ExecuteTemplate(w, name, data)
+	if err != nil {
+		http.Error(w, "No es posible retornar el template", http.StatusInternalServerError)
+	}
+}*/
